f0c9ecc54323bd4266859102943d25ee99890baa
yejia.995 <yejia.995@bytedance.com>
2025-08-29 09:53:23 +0800

    test

--- hyprbars/barDeco.cpp
+++ hyprbars/barDeco.cpp
@@ -8,7 +8,8 @@
 #include <hyprland/src/render/Renderer.hpp>
 #include <hyprland/src/managers/LayoutManager.hpp>
 #include <hyprland/src/config/ConfigManager.hpp>
-#include <hyprland/src/managers/AnimationManager.hpp>
+#include <hyprland/src/managers/animation/AnimationManager.hpp>
+#include <hyprland/src/managers/animation/DesktopAnimationManager.hpp>
 #include <hyprland/src/protocols/LayerShell.hpp>
 #include <pango/pangocairo.h>
 
